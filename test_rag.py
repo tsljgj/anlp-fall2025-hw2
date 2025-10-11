29737259287125afe80e2f60748b7efe1f7743db
tsljgj <tsljgj@outlook.com>
2025-10-11 15:22:07 -0400

    updated test_rag

--- test_rag.py
+++ test_rag.py
@@ -5,6 +5,8 @@ from rag_system.reader.qa_reader import QAReader
 from rag_system.pipeline.rag_pipeline import RAGPipeline
 from rag_system.reader.llm_reader import LLMReader
 
+TOGETHER_API_KEY = "3f384ce9f4a4b21580d2fea88ef7c8dc978b6a4800596d17e3d1ad4dda1a962e"
+
 corpus = [
     "Pittsburgh was named after William Pitt.",
     "The first ICML conference was held in Pittsburgh in 1980."
@@ -13,8 +15,8 @@ corpus = [
 bm25 = BM25Retriever(corpus)
 dense = DenseRetriever(corpus)
 hybrid = HybridRetriever(dense, bm25, alpha=0.6)
-reader = LLMReader(model_name="Qwen/Qwen2.5-Coder-32B-Instruct")
 
+reader = LLMReader(model_name="Qwen/Qwen2.5-Coder-32B-Instruct", api_key=TOGETHER_API_KEY)
 
 rag = RAGPipeline(hybrid, reader)
 result = rag.run("Who is Pittsburgh named after?")
